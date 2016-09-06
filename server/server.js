2a69bf0911531689a0c323d99a968f4d5bb9d441
oliverbrummel <oliverbrummel1@gmail.com>
2016-09-06 17:33:46 -0500

    add MongoDB config to server.js -- still need to set up data models though

--- server/server.js
+++ server/server.js
@@ -1,21 +1,37 @@
 var express = require('express');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var path = require('path');//not sure if this is necessary...
+var passport = require('passport');
+var localStrategy = require('passport-local');
 var bodyParser = require('body-parser');
-//routes
+//[[[[[[[[[[[[[[[[[[[ REQUIRED ROUTES ]]]]]]]]]]]]]]]]]]]
 var indexRouter = require('./routes/index.js');
 
 
-
+//[[[[[[[[[[[[[[[[ EXPRESS ]]]]]]]]]]]]]]]]
 var app = express();
-var port = process.env.PORT || 3000;
 
-
+app.use(express.static('server/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('server/public'));
-
+//[[[[[[[[[[[[[[[[ ROUTE PATHS ]]]]]]]]]]]]]]]]
 app.use('/', indexRouter);
 
+//[[[[[[[[[[[[[[[[[[[ mongoDB ]]]]]]]]]]]]]]]]]]]
+var mongoURI = 'mongodb://localhost/v2_comic_entries';
+var MongoDB = mongoose.connect(mongoURI).connection;
 
+MongoDB.on('error', function(err){
+  console.log('MongoDB connection error:', err);
+})
+
+MongoDB.once('open', function(){
+  console.log('MongoDB connection open!');
+})
+
+
+var port = process.env.PORT || 3000;
 var server = app.listen(port, function(){
   var liveport = server.address().port;
   console.log('Now listening on port:', liveport + '. Press ctrl-c to end.');
